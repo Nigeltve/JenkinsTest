64008e992fd23d8884e1be10373b2fe6be0d50b4
Nigel Thornewill von Essen <nigel@thornewill.org>
2025-11-25 00:49:46 +0100

    fake failing test

---
+++ Tests/DummyFailing.cs
@@ -0,0 +1,13 @@
+using FluentAssertions;
+
+namespace Tests;
+
+public class DummyFailing
+{
+	[Fact]
+	private void FailingTest()
+	{
+		const bool value = true;
+		value.Should().BeFalse();
+	}
+}
